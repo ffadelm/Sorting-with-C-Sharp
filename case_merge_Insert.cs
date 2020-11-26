311318c29c65d30d2f512fb86afca334eb0d4e25
Faysal Fadel Maulana <67732382+ffadelm@users.noreply.github.com>
2020-11-26 19:04:19 +0700

    Update case_merge_Insert.cs

--- case_merge_Insert.cs
+++ case_merge_Insert.cs
@@ -1,41 +1,102 @@
 class SortingNama
     {
-        //deklarasi array dengan batas 26
+        //membuat batas arr 26   
         string[] fay = new string[26];
         //deklarasi int max sebagai jumlah elemen dlam array
         int max;
-        void InputNama()
+                   
+        void InputOutput()
         {
-            while (true)
-            {
-                Console.Write("Masukkan jumlah Elemen : ");
-                string y = Console.ReadLine();
-                max = Int32.Parse(y);
-                if (max <= 26)
-                    break;
-                else
-                    Console.WriteLine("\nMohon Maaf Batas Element Array Hanya 26\n");
-            }
+            //deklarasi pilih, ulang untuk mengulang menu pilihan
+            int pilih;
+            string ulang;
+            ulang = "Y";
 
-            Console.WriteLine("========================================");
-
-            //menyimpan element array
-            for (int i = 0; i < max; i++)
+            while(ulang == "Y")
             {
-                Console.Write(" Masukkan Nama ke [" + (i + 1) + "] : ");
-                fay[i] = Console.ReadLine();
-            }
-                        
+                Console.WriteLine("========================================");
+                Console.WriteLine("Selamat Datang di Program Sorting Nama");
+                Console.WriteLine("========================================\n");
+
+                
+                while (true)
+                {
+                    Console.Write(" Masukkan Jumlah Siswa : ");
+                    string y = Console.ReadLine();
+                    max = Int32.Parse(y);
+                    if (max <= 26) //max tidak boleh lebih dari 26                 
+                        break;
+                    else
+                        Console.WriteLine("\nbatas Maksimal Jumlah Siswa hanya 26\n");
+                        Console.WriteLine("***************************************");
+                }
+
+                // buat arr dengan batas nilai max
+                string[] fay = new string[max];
+                Console.WriteLine("========================================");
+                Console.WriteLine("Masukkan Nama Siswa : ");
+                for (int i = 0; i < max; i++)
+                {
+                    // looping nama siswa
+                    Console.Write((i + 1) + ". ");
+                    //menyimpan elemen arr
+                    fay[i] = Console.ReadLine();
+                }
+
+                //program switch case atau menu pilihan
+                a://isi a 
+                Console.WriteLine("\n >>>>> Pilihan Metode Sorting <<<<< ");
+                Console.WriteLine("\n 1. Insertion Sort");
+                Console.WriteLine(" 2. Merge Sort");
+                Console.WriteLine(" 3. Exit");
+                Console.Write(" Pilihan (1/2/3) : ");
+                pilih = Int32.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                switch (pilih)
+                {
+                    //case 1 berisi sorting insertion
+                    case 1:
+                        //proses insertion sorting 
+                        InsertSort(fay, max);
+                        //menampilkan elemen yang telah diurutkan
+                        Tampil(fay, pilih);
+                        break;
+
+                    //case2 berisi sorting merge
+                    case 2:
+                        //proses Merge sorting 
+                        //sekaligus menampilkan elemen yang telah diurutkan
+                        Tampil(SortMerge(fay), pilih);
+                        break;
+                    // Exit
+                    case 3:
+                        return;
+                    //Wrong input
+                    default:
+                        Console.WriteLine("Pilihan Salah !!!!");
+                        Console.WriteLine("Masukkan pilihan 1-3");
+                        Console.WriteLine("***************************************");
+                        goto a;//untuk kembali ke a  
+                }
+                //pilihan untuk keluar konsol atau kembali kemenu
+                Console.Write("\nKembali Kemenu ? (Y/T) : ");
+                ulang = Console.ReadLine();
+                //untuk menghapus 
+                Console.Clear();
+                
+
+            }        
         }
 
-        static string[] InsertSort(string[] fay,int max)
+        static string[] InsertSort(string[] fay, int max)
         {
             //deklarasi int i dan j
             int i, j;
             // 1. Repeat steps 2, 3, 4, and 5 varying i from 1 to n – 1 
             for (i = 1; i < max; i++)
             {
-                // Menginisialisasi temp
+                // set temp
                 string temp = fay[i];
                 // Set j = i – 1
                 j = i - 1;
@@ -53,128 +114,130 @@ class SortingNama
             return fay;
         }
 
-        static public void MainMerge<T>(T[] values, int left, int mid, int right) where T : IComparable<T>
+        static string[] SortMerge(string[] fay)
         {
-            T[] temp = new T[25];
-            int i, eol, num, pos;
+            // hitung nilai a
+            int a = fay.Length;
 
-            eol = (mid - 1);
-            pos = left;
-            num = (right - left + 1);
+            // Batasan rekursi / jika n <= 1, keluarkan array
+            if (a <= 1) 
+                return fay;
 
-            while ((left <= eol) && (mid <= right))
+            // Menentukan nilai mid
+            int mid = a / 2;
+
+            // deklarasi arr A
+            string[] fayA = new string[mid];
+
+            // deklarasi arr B
+            string[] fayB;
+
+            if (a % 2 == 0)
             {
-                if (values[left].CompareTo(values[mid])<1)
-                    temp[pos++] = values[left++];
-                else
-                    temp[pos++] = values[mid++];
+                fayB = new string[mid];
+            }
+            else
+            {
+                fayB = new string[mid + 1];
             }
 
-            while (left <= eol)
-                temp[pos++] = values[left++];
+            // Divide the list into two sublists of nearly equal lengths, and sort each sublist by using merge sort. The steps to do this are as follows:
 
-            while (mid <= right)
-                temp[pos++] = values[mid++];
+            // Algorithm(low, mid) 
+            for (int i = 0; i < mid; i++)
+                fayA[i] = fay[i];
 
-            for (i = 0; i < num; i++)
+            fayA = SortMerge(fayA);
+
+            // Algorithm(mid + 1, high) 
+            for (int i = mid, x = 0; i < a; i++, x++)
             {
-                values[right] = temp[right];
-                right--;
+                fayB[x] = fay[i];
             }
-        }
 
-        static public void SortMerge<T>(T[] values, int left, int right) where T : IComparable<T>
-        {
-            int mid;
+            fayB = SortMerge(fayB);
 
-            if (right > left)
-            {
-                mid = (right + left) / 2;
-                SortMerge(values, left, mid);
-                SortMerge(values, (mid + 1), right);
 
-                MainMerge(values, left, (mid + 1), right);
-            }
-        }
-
-        void mergedisplay()
-        {
-            Console.WriteLine("\n==============================================");
-            Console.WriteLine(">>>>>>>>> Diurutkan dengan MergeSort <<<<<<<<<");
-            Console.WriteLine("==============================================");
-            SortMerge(fay,0,max-1);
-            for (int i = 0; i < max; i++)
-                Console.WriteLine(" " + fay[i]);
+            // Merge the two sorted sublists:
+            return MainMerge(fayA, fayB);
 
         }
 
-        void insertdisplay()
+        static string[] MainMerge(string[] fayA, string[] fayB)
         {
-            Console.WriteLine("\n===================================================");
-            Console.WriteLine(">>>>>>>>> Diurutkan dengan Insertion Sort <<<<<<<<<");
-            Console.WriteLine("===================================================");
+            //set I,J,K
+            int i = 0, FM = 0, k = 0;
             
-            //add elemen yang tersimpan dalam array agar di proses untuk sorting 
-            //memanggil fungsi insertion sort
-            InsertSort(fay,max);
-            for (int i = 0; i < max; i++)
-            Console.WriteLine(" " + fay[i]);
+            int n = fayB.Length + fayA.Length;
+
+            string[] result = new string[n];
+
+            //Repeat until i > len(arrA) or j > len(arrB): 
+            while (i < fayA.Length || FM < fayB.Length)
+            {
+                if (i < fayA.Length && FM < fayB.Length)
+                {
+                    if (fayA[i][0] <= fayB[FM][0]) //If(arr[i] <= arr[j])
+                    {
+                        result[k] = fayA[i]; // Store arr[i] at index k in array B
+                        i++; // Increment i by 1 
+                    }
+                    else
+                    {
+                        result[k] = fayB[FM]; // Store arr[j] at index k in array B
+                        FM++; // Increment j by 1
+                    }
+                    k++; // Increment k by 1
+                }
+                else if (i < fayA.Length)
+                {
+                    result[k] = fayA[i]; //Store arr[j] at index k in array B
+                    i++; //Increment j by 1
+                    k++; //Increment k by 1
+                }
+                else if (FM < fayB.Length)
+                {
+                    result[k] = fayB[FM]; //Store arr[i] at index k in array B
+                    FM++; //Increment I by 1
+                    k++; //Increment k by 1
+                }
+            }
+            return result;
+        }
+
+        void Tampil(String[] fay, int pilih)
+        {
+            if (pilih == 1)
+            {
+                // Output Insertion Sorting
+                Console.WriteLine("\n==============================================");
+                Console.WriteLine(">>>>>>>> Diurutkan dengan InsertSort <<<<<<<<");
+                Console.WriteLine("=============================================");
+
+                for (int i = 0; i < max; i++)
+                {
+                    Console.WriteLine((i + 1) + ". " + fay[i]);
+                }
+            }
+            else if (pilih == 2)
+            {
+                // Output Merge Sorting
+                Console.WriteLine("\n==============================================");
+                Console.WriteLine(">>>>>>>>> Diurutkan dengan MergeSort <<<<<<<<<");
+                Console.WriteLine("==============================================");
+                
+                for (int i = 0; i < max; i++)
+                {
+                    Console.WriteLine((i + 1) + ". " + fay[i]);
+                }
+            }
+            
         }
 
         static void Main(string[] args)
         {
-            //deklarasi objek kelas
             SortingNama ini = new SortingNama();
+            ini.InputOutput();
 
-            int pilih;
-            string ulang;
-            ulang = "Y";
-            
-            //program case atau menu pilihan 
-            while (ulang == "Y")
-            {
-                Console.WriteLine("========================================");
-                Console.WriteLine("Selamat Datang di Program Sorting Nama");
-                Console.WriteLine("========================================");
-                a://isi a 
-                Console.WriteLine("\n Pilihan Metode Sorting : ");
-                Console.WriteLine("\n 1. Insertion Sort");
-                Console.WriteLine(" 2. Merge Sort");
-                Console.WriteLine(" 3. Exit");
-                Console.Write(" pilihan (1/2/3) : ");
-                pilih = Int32.Parse(Console.ReadLine());
-                Console.WriteLine();
-
-                switch (pilih)
-                {
-                    //case 1 berisi sorting insertion
-                    case 1:
-                        //menerima elemen array
-                        ini.InputNama();
-                        //menampilkan array yang di urutkan
-                        ini.insertdisplay();
-                        break;
-
-                    //case2 berisi sorting merge
-                    case 2:
-                        //menerima elemen array
-                        ini.InputNama();
-                        ini.mergedisplay();
-                        break;
-
-                    case 3:
-                        return;
-
-                    default:
-                        Console.WriteLine("Pilihan Salah !!!!");
-                        Console.WriteLine("Masukkan pilihan 1-3");
-                        goto a;//untuk kembali ke a                                              
-                }
-
-                //pilihan untuk keluar konsol atau kembali kemenu
-                Console.Write("\nKembali Kemenu ? (Y/T) : ");
-                ulang = Console.ReadLine();
-                Console.Clear(); //untuk menghapus ketika variable ulang di isi
-            }
         }
     }
